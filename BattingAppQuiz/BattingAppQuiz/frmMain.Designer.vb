58b9f2f9b1a8c5844527351fff557a3163fe0e9d
Evgeniy Petin <34118433+solidmetall@users.noreply.github.com>
2017-12-12 11:42:16 -0800

    Add files via upload

---
+++ BattingAppQuiz/BattingAppQuiz/frmMain.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnEnterData = New System.Windows.Forms.Button()
+        Me.lstName = New System.Windows.Forms.ListBox()
+        Me.lstAtBats = New System.Windows.Forms.ListBox()
+        Me.lstHits = New System.Windows.Forms.ListBox()
+        Me.lstAverage = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.BattingAppQuiz.My.Resources.Resources.Baseball
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(467, 127)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'btnEnterData
+        '
+        Me.btnEnterData.Enabled = False
+        Me.btnEnterData.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEnterData.ForeColor = System.Drawing.Color.DarkRed
+        Me.btnEnterData.Location = New System.Drawing.Point(12, 215)
+        Me.btnEnterData.Name = "btnEnterData"
+        Me.btnEnterData.Size = New System.Drawing.Size(128, 23)
+        Me.btnEnterData.TabIndex = 1
+        Me.btnEnterData.Text = "&Enter Player Data"
+        Me.btnEnterData.UseVisualStyleBackColor = True
+        '
+        'lstName
+        '
+        Me.lstName.ForeColor = System.Drawing.Color.DarkRed
+        Me.lstName.FormattingEnabled = True
+        Me.lstName.Location = New System.Drawing.Point(146, 173)
+        Me.lstName.Name = "lstName"
+        Me.lstName.Size = New System.Drawing.Size(134, 147)
+        Me.lstName.TabIndex = 2
+        '
+        'lstAtBats
+        '
+        Me.lstAtBats.ForeColor = System.Drawing.Color.DarkRed
+        Me.lstAtBats.FormattingEnabled = True
+        Me.lstAtBats.Location = New System.Drawing.Point(286, 173)
+        Me.lstAtBats.Name = "lstAtBats"
+        Me.lstAtBats.Size = New System.Drawing.Size(53, 147)
+        Me.lstAtBats.TabIndex = 3
+        '
+        'lstHits
+        '
+        Me.lstHits.ForeColor = System.Drawing.Color.DarkRed
+        Me.lstHits.FormattingEnabled = True
+        Me.lstHits.Location = New System.Drawing.Point(345, 173)
+        Me.lstHits.Name = "lstHits"
+        Me.lstHits.Size = New System.Drawing.Size(53, 147)
+        Me.lstHits.TabIndex = 4
+        '
+        'lstAverage
+        '
+        Me.lstAverage.ForeColor = System.Drawing.Color.DarkRed
+        Me.lstAverage.FormattingEnabled = True
+        Me.lstAverage.Location = New System.Drawing.Point(404, 173)
+        Me.lstAverage.Name = "lstAverage"
+        Me.lstAverage.Size = New System.Drawing.Size(53, 147)
+        Me.lstAverage.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label1.Location = New System.Drawing.Point(143, 157)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label2.Location = New System.Drawing.Point(283, 157)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "At Bats"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label3.Location = New System.Drawing.Point(342, 157)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Hits"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label4.Location = New System.Drawing.Point(401, 157)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Average"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label5.Location = New System.Drawing.Point(83, 101)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(303, 31)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Team Batting Average"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label6.Location = New System.Drawing.Point(130, 345)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(187, 16)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Team Batting Average is: "
+        '
+        'lblAverage
+        '
+        Me.lblAverage.AutoSize = True
+        Me.lblAverage.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAverage.ForeColor = System.Drawing.Color.DarkRed
+        Me.lblAverage.Location = New System.Drawing.Point(323, 345)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(55, 16)
+        Me.lblAverage.TabIndex = 12
+        Me.lblAverage.Text = "Label7"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(469, 24)
+        Me.MenuStrip1.TabIndex = 13
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ClearToolStripMenuItem.Text = "C&lear"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(469, 376)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstAverage)
+        Me.Controls.Add(Me.lstHits)
+        Me.Controls.Add(Me.lstAtBats)
+        Me.Controls.Add(Me.lstName)
+        Me.Controls.Add(Me.btnEnterData)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "frmMain"
+        Me.Text = "Team Batting Average"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents btnEnterData As System.Windows.Forms.Button
+    Friend WithEvents lstName As System.Windows.Forms.ListBox
+    Friend WithEvents lstAtBats As System.Windows.Forms.ListBox
+    Friend WithEvents lstHits As System.Windows.Forms.ListBox
+    Friend WithEvents lstAverage As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents lblAverage As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ BattingAppQuiz/BattingAppQuiz/frmMain.vb
@@ -0,0 +1,85 @@
+﻿'Batting Quiz
+'Evgeniy Petin
+'CSIT-808
+
+Public Class frmMain
+
+    Dim Input As String
+    Dim Name As String
+    Dim TotalHits, TotalBats, AtBats, AtHits, Average As Decimal
+    
+    Dim Hits, Bats As Integer
+    Const NumOfPlayers As Integer = 9
+    Dim IntCounter As Integer = 1
+
+    Dim Valid As Boolean = False
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblAverage.Text = "0.000"
+        btnEnterData.Enabled = True
+        TotalHits = 0
+        TotalBats = 0
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
+        lstAtBats.Items.Clear()
+        lstAverage.Items.Clear()
+        lstHits.Items.Clear()
+        lstName.Items.Clear()
+        lblAverage.Text = "0.000"
+        btnEnterData.Enabled = True
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnEnterData_Click(sender As Object, e As EventArgs) Handles btnEnterData.Click
+        For var As Integer = 0 To 8
+            Name = InputBox("Enter Player's Name: ", "Player's Name")
+
+            Do
+                Input = InputBox("Enter At Bats: ", "At Bats")
+                If IsNumeric(Input) Then
+                    If Convert.ToInt32(Input) > 0 Then
+                        Valid = True
+                        AtBats = Input
+                    Else
+                        MessageBox.Show("Please Enter Valid Positive Number", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+                Else
+                    MessageBox.Show("Please Enter Valid Positive Number", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            Loop While Not Valid
+
+            Valid = False
+
+            Do
+                Input = InputBox("Enter Hits: ", "Hits")
+                If IsNumeric(Input) Then
+                    If Convert.ToInt32(Input) > 0 Then
+                        Valid = True
+                        Hits = Input
+                    Else
+                        MessageBox.Show("Please Enter Valid Positive Number", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+                Else
+                    MessageBox.Show("Please Enter Valid Positive Number", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            Loop While Not Valid
+            Valid = False
+            'AtBats = InputBox("Enter At Bats: ", "At Bats")
+            ' Hits = InputBox("Enter Hits: ", "Hits")
+            lstName.Items.Add(Name)
+            lstAtBats.Items.Add(AtBats)
+            lstHits.Items.Add(Hits)
+            lstAverage.Items.Add((Hits / AtBats).ToString("f3"))
+            TotalHits += Hits
+            TotalBats += AtBats
+            lblAverage.Text = (TotalHits / TotalBats).ToString("f3")
+        Next
+        btnEnterData.Enabled = False
+    End Sub
+
+    
+End Class
